698065add7c2314bd389982eb4a61a72dd3e9e95
Pracheta Boddavaram Amaranath <pboddavarama@swarm2.cm.cluster>
2019-11-12 23:11:09 -0500

    adding all files

--- src/Defects4J_Experiment/all_sequencer.sh
+++ src/Defects4J_Experiment/all_sequencer.sh
@@ -1,5 +1,5 @@
-echo "Reading from sequencer_metadata.csv"
+echo "Reading from sequencer_rerun.csv"
 while IFS=, read -r col1 col2 col3 col4
 do
 	sbatch npr_job.sh $col1 $col2 $col3 $col4
-done < sequencer_metadata.csv
+done < sequencer_rerun.csv

8122cf63ae154b806031dbe1dd4117d12dc18f2d
Manish Motwani <mmotwani@cs.umass.edu>
2019-11-26 21:59:03 -0500

    CNN arch

--- results/NPR_model/train.sh
+++ results/NPR_model/train.sh
@@ -9,5 +9,5 @@ if [ ! -d $data_path ]; then
 	data_path=`/bin/pwd`
 fi
 cd $OpenNMT_py
-python train.py -data $data_path/final -encoder_type cnn -enc_layers 2 -decoder_type cnn -dec_layers 2 -cnn_kernel_width 3 -global_attention general -batch_size 32 -word_vec_size 256 -bridge -copy_attn -reuse_copy_attn -train_steps 20000 -gpu_ranks 0 -save_checkpoint_steps 10000 -save_model $data_path/final-model > $data_path/train.final.out
+python train.py -data $data_path/final -encoder_type cnn -enc_layers 2 -decoder_type cnn -dec_layers 2 -cnn_kernel_width 3 -global_attention general -batch_size 32 -word_vec_size 256 -bridge -train_steps 20000 -gpu_ranks 0 -save_checkpoint_steps 10000 -save_model $data_path/final-model > $data_path/train.final.out
 echo "train.sh complete" >> $data_path/train.out
